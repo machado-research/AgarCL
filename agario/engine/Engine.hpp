8eb4cba61d578a46a29c6fa4f7180a942fffd96c
Mohamed Ayman <mohamedayman15069@aucegypt.edu>
2024-06-03 11:54:06 -0600

    refactoring

--- agario/engine/Engine.hpp
+++ agario/engine/Engine.hpp
@@ -193,7 +193,7 @@ namespace agario {
     void add_viruses(int n) {
       agario::distance virus_radius = agario::radius_conversion(VIRUS_MASS);
       for (int v = 0; v < n; v++)
-        state.viruses.emplace_back(random_location(virus_radius));
+        state.viruses.emplace_back(random_location(virus_radius ));
     }
 
     /**
