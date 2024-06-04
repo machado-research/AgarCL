5c57a9591fca1983b9b40f4eed1693fcc515f248
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-06-04 15:57:17 -0400

    avoid glad and openGL conflicts

--- agario/rendering/shader.hpp
+++ agario/rendering/shader.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include "glad/glad.h"
+#include <GLFW/glfw3.h>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #define GL_SILENCE_DEPRECATION
-#include <GLFW/glfw3.h>
-
 class ShaderException : public std::runtime_error {
   using runtime_error::runtime_error;
 };
