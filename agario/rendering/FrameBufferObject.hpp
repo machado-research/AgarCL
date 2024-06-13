1138f8497daa054b0f1ab8879d98f6a69b41186c
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-06-13 13:56:37 -0400

    fix segmentation fault

--- agario/rendering/FrameBufferObject.hpp
+++ agario/rendering/FrameBufferObject.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "agario/rendering/platform.hpp"
+// #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 #include "agario/rendering/Canvas.hpp"
+#include <iostream>
 
 class FBOException : public std::runtime_error {
-  using runtime_error::runtime_error;
+  // using runtime_error::runtime_error;
+  public:
+    using std::runtime_error::runtime_error;
+
+    FBOException(const std::string &message) : std::runtime_error(message) {
+        std::cout << "FBOException: " << message << std::endl;
+    }
+
+    FBOException(const char *message) : std::runtime_error(message) {
+        std::cout << "FBOException: " << message << std::endl;
+    }
 };
 
 void glfw_error_callback(int error, const char *description) {
@@ -24,8 +39,8 @@ public:
     fbo(0), rbo_depth(0), rbo_color(0),
     window(nullptr) {
 
-    glfwSetErrorCallback(glfw_error_callback);
 
+    glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
       throw FBOException("GLFW initialization failed.");
 
@@ -36,7 +51,8 @@ public:
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    // glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    // std::cout << "Creating off-screen window" << std::endl;
     window = glfwCreateWindow(_width, _height, "", nullptr, nullptr);
 
     if (window == nullptr) {
@@ -44,9 +60,22 @@ public:
       throw FBOException("Off-screen window creation failed");
     }
 
-    glfwHideWindow(window);
+    // glfwHideWindow(window);
     glfwMakeContextCurrent(window);
 
+    if (!glfwGetCurrentContext()) {
+            throw FBOException("Failed to make context current");
+        }else{
+          std::cout << "glfwGetCurrentContext ok " << std::endl;
+        }
+    
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        throw FBOException("Failed to initialize GLAD" );
+    }else{
+      std::cout << "GLAD ok " << std::endl;
+
+    }
     // Frame Buffer Object
     glGenFramebuffers(1, &fbo);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
@@ -62,6 +91,7 @@ public:
     glBindRenderbuffer(GL_RENDERBUFFER, rbo_depth);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, _width, _height);
     glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rbo_depth);
+    
 
     auto fbo_status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
     if (fbo_status == GL_FRAMEBUFFER_UNSUPPORTED)
@@ -85,15 +115,20 @@ public:
 
   void copy(void *data) {
     glReadBuffer(GL_COLOR_ATTACHMENT0);
+    // glReadPixels(_width / 2, _height / 2, _width, _height, GL_RGB, GL_UNSIGNED_BYTE, data);
     glReadPixels(_width / 2, _height / 2, _width, _height, GL_RGB, GL_UNSIGNED_BYTE, data);
+    // glReadPixels(0, 0, _width, _height, GL_RGB, GL_UNSIGNED_BYTE, data);
   }
 
   void swap_buffers() const { glfwSwapBuffers(window); }
 
   ~FrameBufferObject() override {
+    // std::cout << "FrameBufferObject destructor called" << std::endl;
     glDeleteFramebuffers(1, &fbo);
     glDeleteRenderbuffers(1, &rbo_color);
     glDeleteRenderbuffers(1, &rbo_depth);
+    glfwDestroyWindow(window);
+    glfwTerminate();
   }
 
 private:
