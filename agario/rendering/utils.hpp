cdd2289b67f38990aaf1dc47bb8c1dddeff4ff3a
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-06-18 17:08:01 -0400

    create utils.hpp and move some error messages

---
+++ agario/rendering/utils.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+class FBOException : public std::runtime_error {
+  // using runtime_error::runtime_error;
+  public:
+    using std::runtime_error::runtime_error;
+
+    FBOException(const std::string &message) : std::runtime_error(message) {
+        std::cout << "FBOException: " << message << std::endl;
+    }
+
+    FBOException(const char *message) : std::runtime_error(message) {
+        std::cout << "FBOException: " << message << std::endl;
+    }
+};
+
+
+void exception_on_gl_error(std::string error){
+  auto glstatus = glGetError();
+    if (glstatus != GL_NO_ERROR)
+    {
+      std::string es((const char *)gluErrorString(glstatus));
+      throw FBOException("GL Error in copy "+ error + ' '+ std::to_string(glstatus)+ ", "+ es);
+      }
+}
