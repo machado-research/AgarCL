f2135170df540c15c0a9520f13a6cff17cce75dd
Mohamed Mohamed <mayman@cedar1.int.cedar.computecanada.ca>
2025-01-30 21:30:57 -0800

    change location

--- agario/engine/Engine.hpp
+++ agario/engine/Engine.hpp
@@ -107,10 +107,12 @@ namespace agario {
         // auto random_index = random<agario::distance>(0, state.pellets.size() - 1);
         auto random_index = 0;
         auto loc = state.pellets[random_index].location();
-        loc.x += 2*agario::radius_conversion(CELL_MIN_SIZE);
-        loc.y += 2*agario::radius_conversion(CELL_MIN_SIZE);
-        loc.x = std::min(loc.x, arena_width() - agario::radius_conversion(CELL_MIN_SIZE));
-        loc.y = std::min(loc.y, arena_height() - agario::radius_conversion(CELL_MIN_SIZE));
+        // loc.x += 2*agario::radius_conversion(CELL_MIN_SIZE);
+        // loc.y += 2*agario::radius_conversion(CELL_MIN_SIZE);
+        // loc.x = std::min(loc.x, arena_width() - agario::radius_conversion(CELL_MIN_SIZE));
+        // loc.y = std::min(loc.y, arena_height() - agario::radius_conversion(CELL_MIN_SIZE));
+        loc.x +=10; 
+        loc.y +=10;
         player.add_cell(loc, CELL_MIN_SIZE);
         // player.add_cell(state.pellets[random_index].location(), CELL_MIN_SIZE);
       } else {
